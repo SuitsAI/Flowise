8ff1de34428ff9802580af66e2db5c7e6cfd57bc
jonSuits <jonathan@suits.ai>
2025-11-19 18:28:44 -0500

    Add Models to model json and redirect to it

--- packages/components/src/modelLoader.ts
+++ packages/components/src/modelLoader.ts
@@ -34,7 +34,8 @@ const isValidUrl = (urlString: string) => {
  * If any of the loading fails, fallback to the default models.json file on disk
  */
 const getRawModelFile = async () => {
-    const modelFile = process.env.MODEL_LIST_CONFIG_JSON
+    //        process.env.MODEL_LIST_CONFIG_JSON ?? 'https://raw.githubusercontent.com/FlowiseAI/Flowise/main/packages/components/models.json'
+    const modelFile = process.env.MODEL_LIST_CONFIG_JSON ?? getModelsJSONPath()
     try {
         if (modelFile && isValidUrl(modelFile)) {
             const resp = await axios.get(modelFile)
