543ce33c6f94b7e8fe2015f1ec302897194d3f9b
man2201 <panasyuk@brights.io>
2026-03-12 17:03:45 +0200

    Update CodeInterpreterE2B.ts

--- packages/components/nodes/tools/CodeInterpreterE2B/CodeInterpreterE2B.ts
+++ packages/components/nodes/tools/CodeInterpreterE2B/CodeInterpreterE2B.ts
@@ -113,7 +113,7 @@ class Code_Interpreter_Tools implements INode {
             name: toolName ?? NAME,
             apiKey: e2bApiKey,
             schema: z.object({
-                command: z.string().describe('Command to be executed in the sandbox environment before executing the input code'),
+                command: z.string().describe('Command to be executed in the sandbox environment before executing the input code (to install packages, etc)'),
                 input: z.string().describe('Python code to be executed in the sandbox environment')
             }),
             chatflowid: options.chatflowid,
